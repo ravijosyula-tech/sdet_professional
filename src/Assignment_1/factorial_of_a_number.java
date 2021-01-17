a9889cb3ec9d11cf48a3ca97a6a3a617afca8b2c
JosyulaR <JosyulaR@HBADT4950216.bsg.ad.adp.com>
2021-01-17 21:33:18 +0530

    Assignments for commit

---
+++ src/Assignment_1/factorial_of_a_number.java
@@ -0,0 +1,18 @@
+package Assignment_1;
+
+public class factorial_of_a_number {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        
+        int num = 5, i = 1;
+  long factorial = 1;
+  while(i <= num)
+  {
+      factorial *= i;
+      i++;
+  }
+  System.out.printf("Factorial of %d = %d", num, factorial);
+}
+}
